5c7a255cfc75e55dda07fccfdfab403f3bc189dc
michecaizza-star <michecaizza@gmail.com>
2026-08-21 16:11:49 +0200

    Configura export per GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,13 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
